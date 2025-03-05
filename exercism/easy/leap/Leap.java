feddb30f81419750af11ff976aff717d67e4cddf
Kirill Chausov <kirill.chausov2@gmail.com>
2025-03-05 12:34:51 +0300

    Add leap solution

---
+++ exercism/easy/leap/Leap.java
@@ -0,0 +1,10 @@
+package easy.leap;
+
+// Link: https://exercism.org/tracks/java/exercises/leap
+
+public class Leap {
+
+    boolean isLeapYear(int year) {
+        return year % 4 == 0 && (!(year % 100 == 0) || (year % 400 == 0));
+    }
+}
