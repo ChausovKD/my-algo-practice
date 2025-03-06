74300191001b3080afba5ebdd28843d93a89c268
Kirill Chausov <kirill.chausov2@gmail.com>
2025-03-06 19:08:08 +0300

    Solve difference-of-squares (exercism easy)

---
+++ exercism/easy/difference_of_squares/DifferenceOfSquaresCalculator.java
@@ -0,0 +1,26 @@
+package easy.difference_of_squares;
+
+// Link: https://exercism.org/tracks/java/exercises/difference-of-squares
+
+public class DifferenceOfSquaresCalculator {
+
+    int computeSquareOfSumTo(int input) {
+        int sum = 0;
+        for (int i = 1; i <= input; i++) {
+            sum += i;
+        }
+        return sum * sum;
+    }
+
+    int computeSumOfSquaresTo(int input) {
+        int sumOfSquares = 0;
+        for (int i = 1; i <= input; i++) {
+            sumOfSquares += i * i;
+        }
+        return sumOfSquares;
+    }
+
+    int computeDifferenceOfSquares(int input) {
+        return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
+    }
+}
