d9228aee884823f5e944a2e6db5b807aa5208d0b
Kirill Chausov <kirill.chausov2@gmail.com>
2025-03-07 20:06:41 +0300

    wizards-and-warriors-2 - solution

---
+++ exercism/easy/wizards_and_warriors_2/Destination.java
@@ -0,0 +1,23 @@
+package easy.wizards_and_warriors_2;
+
+public class Destination {
+
+    private String name;
+    private int inhabitants;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getInhabitants() {
+        return inhabitants;
+    }
+
+    public void setInhabitants(int inhabitants) {
+        this.inhabitants = inhabitants;
+    }
+}
